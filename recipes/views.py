4eaddb973f64fb7c5d2521d2a9ad5fcd537e3a17
Chris <chrisfriday033@gmail.com>
2024-06-18 21:48:50 +0100

    Created the base.html file

--- recipes/views.py
+++ recipes/views.py
@@ -1,7 +1,11 @@
 from django.shortcuts import render
-from django.views.generic import TemplateView
+from django.views.generic import TemplateView, ListView
+from .models import Dish
 
 # Create your views here.
 
 class HomeView(TemplateView):
     template_name='recipes/home.html'
+
+class RecipesListView(ListView):
+    model=Dish
